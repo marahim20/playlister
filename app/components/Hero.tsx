7baffead75bcf60e4f1b8913c0883aa823403cbd
marahim20 <minhajabdulrahim@gmail.com>
2023-07-18 00:15:23 +0530

    fix:minor tweaks in hero section2

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -6,10 +6,10 @@ import { PiYoutubeLogoFill } from 'react-icons/pi';
 export default function Hero() {
     return (
         <section className="flex flex-col md:flex-row items-center justify-center h-3/4">
-            <Image src="/main_illustration.svg" width={400} height={400} alt="main_image" priority />
+            <Image src="/main_illustration.svg" width={400} height={400} alt="main_image" priority className="select-none" />
             <div className="flex flex-col gap-4">
-                <h1 className="font-heading text-6xl text-primary">PlayLister</h1>
-                <p className="font-body text-base max-w-xs text-text">
+                <h1 className="font-heading text-6xl text-primary select-none">PlayLister</h1>
+                <p className="font-body text-base max-w-xs text-text select-none">
                     This app helps you to calculate the total length of any <b>YouTube</b> playlist. Just paste the URL of the playlist or any video from the playlist below and hit Go!
                 </p>
                 <div className="flex gap-4">
