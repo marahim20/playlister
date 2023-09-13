e6282cd3b0477e45fa38ea5417a7bce0a114eec9
marahim20 <minhajabdulrahim@gmail.com>
2023-09-13 13:57:03 +0530

    feat: added twitter-image

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,38 +1,54 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Grandstander, Raleway } from 'next/font/google'
-import { Analytics } from '@vercel/analytics/react';
+import "./globals.css";
+import type { Metadata } from "next";
+import { Grandstander, Raleway } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
-const grandstander = Grandstander({ subsets: ['latin'], variable: '--font-heading' })
-const raleway = Raleway({ subsets: ['latin'], variable: '--font-body' })
+const grandstander = Grandstander({
+  subsets: ["latin"],
+  variable: "--font-heading",
+});
+const raleway = Raleway({ subsets: ["latin"], variable: "--font-body" });
 
 export const metadata: Metadata = {
-  title: 'PlayLister',
-  description: 'This app helps you to calculate the total duration of a YouTube playlist.',
-}
+  title: "PlayLister",
+  description:
+    "This app helps you to calculate the total duration of a YouTube playlist.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <head>
-        <meta property='og:title' content='PlayLister' />
-        <meta property='og:description' content='This app helps you to calculate the total duration of a YouTube playlist.' />
-        <meta property='og:image' content='<generated>' />
+        <meta property="og:title" content="PlayLister" />
+        <meta
+          property="og:description"
+          content="This app helps you to calculate the total duration of a YouTube playlist."
+        />
+        <meta property="og:image" content="<generated>" />
         <meta property="og:image:type" content="<generated>" />
-        <meta property='og:image:width' content='1200' />
-        <meta property='og:image:height' content='630' />
-        <meta property='og:url' content='https://ytplaylister.vercel.app/' />
-        <meta property='og:type' content='website' />
-        <meta property='og:site_name' content='PlayLister' />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:url" content="https://ytplaylister.vercel.app/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="PlayLister" />
+        <meta property="twitter:image" content="<generated>" />
+        <meta property="twitter:title" content="PlayLister" />
+        <meta
+          property="twitter:description"
+          content="This app helps you to calculate the total duration of a YouTube playlist."
+        />
       </head>
-      <body suppressHydrationWarning={true} className={` ${grandstander.variable} ${raleway.variable}`}>
+      <body
+        suppressHydrationWarning={true}
+        className={` ${grandstander.variable} ${raleway.variable}`}
+      >
         {children}
         <Analytics />
       </body>
     </html>
-  )
+  );
 }
